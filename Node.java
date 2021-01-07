473ec2083ad5c5b963f67c0eec8124b05e7f3c17
sging20 <sging20@stuy.edu>
2021-01-07 13:04:36 -0500

    Constructor

---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node{
+  private String data; //data of itself store address, reference to next and previous
+  private Node next,prev; //location
+
+public Node(String input){
+  data= input;
+  next= null;
+  prev= null;
+}
+
+}
